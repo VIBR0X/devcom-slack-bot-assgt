23d73cfe3b8eec5399278489da6ce3a6253dd2e8
Amit Kumar <103894389+Amit-Rohila33@users.noreply.github.com>
2023-02-27 09:55:33 +0530

    adding the birthday [python script to github

---
+++ automated_birthday.py
@@ -0,0 +1,27 @@
+import os
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+from datetime import datetime
+
+# Set the bot token and channel name
+client = WebClient(token=os.environ['xoxb-4861105113075-4884936352320-4sNd9hbTh07xe3JOxLw9ZXl7'])
+channel = "#general"
+
+# Get today's date
+today = datetime.today().strftime('%m-%d')
+
+# Loop through all the users in the workspace
+try:
+    response = client.users_list()
+    users = response['members']
+    for user in users:
+        # Get the user's birthday if available
+        if 'birthday' in user['profile']:
+            birthday = user['profile']['birthday']
+            if birthday == today:
+                # Send a birthday message to the channel
+                client.chat_postMessage(channel=channel, text=f"🎂 Happy birthday <@{user['id']}>! 🎉")
+except SlackApiError as e:
+    print(f"Error: {e}")
+
+
